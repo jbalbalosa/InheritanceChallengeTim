f7fa02cf1e5412dd91529ac7fa623ec9cb28d841
Jeffrey Balbalosa <31784615+jbalbalosa@users.noreply.github.com>
2024-09-04 15:56:44 +0900

    Update HourlyEmployee.java

--- src/HourlyEmployee.java
+++ src/HourlyEmployee.java
@@ -1,4 +1,23 @@
 package PACKAGE_NAME;
 
-public class HourlyEmployee {
+public class HourlyEmployee extends Employee {
+
+    private double hourlyPayRate;
+
+    public HourlyEmployee(String name, String birthDate, String hireDate,
+                          double hourlyPayRate) {
+        super(name, birthDate, hireDate);
+        this.hourlyPayRate = hourlyPayRate;
+    }
+
+    @Override
+    public double collectPay(){
+        return 40 * hourlyPayRate;
+    }
+
+    public double getDoublePay(){
+        return 2 * collectPay();
+    }
+
 }
+
